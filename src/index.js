ba2921d4bf6a2c129f97ee905fc5ea8535ba5547
Koomplo <mariomarcio12324@gmail.com>
2023-07-21 15:11:50 -0300

    Finalizado requisito 01.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const fs = require('fs/promises');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
+
+const talkerPathResolver = path.resolve(__dirname, 'talker.json');
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
@@ -16,3 +20,10 @@ app.listen(PORT, () => {
 });
 
 // Inicio do Projeto.
+
+const readFile = async () => (JSON.parse(await fs.readFile(talkerPathResolver, 'utf-8')));
+
+app.get('/talker', async (_req, res) => {
+  const talker = await readFile();
+  res.status(200).json(talker);
+});
